18deb13c5fc046bae9ed0a342d31ba41907c0ddb
keepnewbie <nuraly042000>
2024-01-22 16:16:07 +0000

    Replacing aosp clang source
    Signed-off-by: keepnewbie <nuraly042000>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
 git clone --depth=1 https://github.com/keepnewbie/vendor-xiaomi-vince -b afterlife vendor/xiaomi/vince
 git clone --depth=1 https://github.com/keepnewbie/kernel_xiaomi_vince -b nksu kernel/xiaomi/vince
-git clone --depth=1 https://gitlab.com/cherishos-tiramisu/vince-clang-17.git prebuilts/clang/host/linux-x86/clang-aosp
+git clone --depth=1 https://gitlab.com/anandhan07/aosp-clang -b 17 prebuilts/clang/host/linux-x86/clang-aosp
