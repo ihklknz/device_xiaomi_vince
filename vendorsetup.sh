d1ef997262d449ba7ff2414e0490ff5cf3805147
keepnewbie <nuraly042000>
2024-01-24 12:11:06 +0000

    track Clang from mysaus
    Signed-off-by: keepnewbie <nuraly042000>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
 git clone --depth=1 https://github.com/keepnewbie/vendor-xiaomi-vince -b afterlife vendor/xiaomi/vince
 git clone --depth=1 https://github.com/keepnewbie/kernel_xiaomi_vince -b nksu kernel/xiaomi/vince
-git clone --depth=1 https://gitlab.com/anandhan07/aosp-clang -b 17 prebuilts/clang/host/linux-x86/clang-aosp
+git clone --depth=1 https://gitlab.com/abdillahsuluki7/aosp-clang -b 17 prebuilts/clang/host/linux-x86/clang-aosp
