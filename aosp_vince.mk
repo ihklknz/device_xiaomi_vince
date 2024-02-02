c54ea81ea911dc58a188a4ebef3d79b7cf8c3ee5
keepnewbie <nuraly042000@gmail.com>
2024-02-02 16:30:10 +0000

    vince Go Horizon
    Signed-off-by: keepnewbie <nuraly042000@gmail.com>

---
+++ aosp_vince.mk
@@ -0,0 +1,47 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# All components inherited here go to system_ext image
+$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
+
+# Inherit from vince device
+$(call inherit-product, device/xiaomi/vince/device.mk)
+
+# Inherit Extra Viper4AFX
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
+
+# Inherit some common Afterlife stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Horizon flags
+HORIZON_MAINTAINER := AL
+CUSTOM_BUILD_TYPE := OFFICIAL
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := vince
+PRODUCT_NAME := aosp_vince
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi 5 Plus
+PRODUCT_MANUFACTURER := Xiaomi
+TARGET_VENDOR := Xiaomi
+BOARD_VENDOR := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
