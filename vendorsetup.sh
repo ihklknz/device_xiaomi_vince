efde1c96ca9d14f1be466f0fa30fdbf321fa349c
keepnewbie <nuraly042000@gmail.com>
2024-02-02 17:20:16 +0000

    vince: vendorsetup: Update repos for A14
    Signed-off-by: keepnewbie <nuraly042000@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-git clone --depth=1 https://github.com/keepnewbie/vendor-xiaomi-vince -b afterlife vendor/xiaomi/vince
+git clone --depth=1 https://github.com/keepnewbie/vendor-xiaomi-vince -b 14 vendor/xiaomi/vince
 git clone --depth=1 https://github.com/keepnewbie/kernel_xiaomi_vince -b main kernel/xiaomi/vince
 git clone --depth=1 https://gitlab.com/abdillahsuluki7/aosp-clang -b 17 prebuilts/clang/host/linux-x86/clang-aosp
 git clone --depth=1 https://github.com/keepnewbie/ViPER4AndroidFX -b main packages/apps/ViPER4AndroidFX
